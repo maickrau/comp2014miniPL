c4193ba1848a024385ad7930de71890ea2cf9455
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-02-28 18:06:34 +0200

    empty project on visual studio

---
+++ comp2014minipl/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comp2014minipl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
