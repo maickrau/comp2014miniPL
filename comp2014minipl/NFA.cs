cf0b100d8d50555771233c2fd6de65286be43fbc
Mikko Rautiainen <m_rautiainen@hotmail.com>
2014-02-28 21:27:20 +0200

    Last commit should have had tests, added now

---
+++ comp2014minipl/NFA.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace comp2014minipl
+{
+    public class NFA
+    {
+        private class Connection
+        {
+            public int start;
+            public int end;
+            public char character; // '\0' is epsilon
+            public Connection(Connection second)
+            {
+                this.start = second.start;
+                this.end = second.end;
+                this.character = second.character;
+            }
+            public Connection(int start, int end, char character)
+            {
+                this.start = start;
+                this.end = end;
+                this.character = character;
+            }
+        }
+        private int startState;
+        private int numStates;
+        private List<int> endStates;
+        private List<Connection> connections;
+        private List<List<Connection>> connectionPerState;
+        private bool hasEpsilons;
+        public void debugPrint()
+        {
+            System.Console.WriteLine(numStates);
+            System.Console.WriteLine(startState);
+            foreach (int i in endStates)
+            {
+                System.Console.Write("{0} ", i);
+            }
+            System.Console.WriteLine();
+            foreach (Connection c in connections)
+            {
+                System.Console.WriteLine("{0} {1} {2}", c.start, c.end, c.character);
+            }
+        }
+        public NFA(String str)
+        {
+            endStates = new List<int>();
+            connections = new List<Connection>();
+            startState = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                connections.Add(new Connection(i, i + 1, str[i]));
+            }
+            numStates = str.Length + 1;
+            endStates.Add(str.Length);
+            hasEpsilons = false;
+            calculateConnectionPerState();
+        }
+        private int addState(bool endState)
+        {
+            int ret = numStates;
+            numStates++;
+            connectionPerState.Add(new List<Connection>());
+            if (endState)
+            {
+                endStates.Add(ret);
+            }
+            return ret;
+        }
+        private void connect(int start, int end, char character)
+        {
+            Connection newConnection = new Connection(start, end, character);
+            connections.Add(newConnection);
+            connectionPerState[start].Add(newConnection);
+        }
+        private void calculateConnectionPerState()
+        {
+            connectionPerState = new List<List<Connection>>();
+            for (int i = 0; i < numStates; i++)
+            {
+                connectionPerState.Add(new List<Connection>());
+            }
+            foreach (Connection c in connections)
+            {
+                connectionPerState[c.start].Add(c);
+            }
+        }
+        public NFA(NFA second)
+        {
+            //we want to create a deep copy instead of modifying the original
+            connections = second.connections.ConvertAll(c => new Connection(c));
+            endStates = new List<int>(second.endStates);
+            startState = second.startState;
+            numStates = second.numStates;
+            hasEpsilons = second.hasEpsilons;
+            calculateConnectionPerState();
+        }
+        private void incrementIndices(int amount)
+        {
+            foreach (Connection c in connections)
+            {
+                c.start += amount;
+                c.end += amount;
+            }
+            for (int i = 0; i < endStates.Count; i++)
+            {
+                endStates[i] += amount;
+            }
+            startState += amount;
+            numStates += amount;
+            calculateConnectionPerState();
+        }
+        public NFA or(NFA second)
+        {
+            NFA ret = new NFA(this);
+            //add states
+            ret.incrementIndices(second.numStates);
+            foreach (int i in second.endStates)
+            {
+                ret.endStates.Add(i);
+            }
+            //add connections between newly added states
+            foreach (Connection c in second.connections)
+            {
+                ret.connections.Add(new Connection(c));
+            }
+            //connect new start states with old start state
+            foreach (Connection c in ret.connections)
+            {
+                if (c.start == second.startState)
+                {
+                    c.start = ret.startState;
+                }
+            }
+            ret.hasEpsilons = hasEpsilons | second.hasEpsilons;
+            ret.calculateConnectionPerState();
+            return ret;
+        }
+        private void addConnectionToEndStates(int destination, char character)
+        {
+            foreach (int i in endStates)
+            {
+                connect(i, destination, character);
+            }
+        }
+        public NFA closure()
+        {
+            NFA ret = new NFA(this);
+            int oldStartState = ret.startState;
+            int newStartState = ret.addState(false);
+            ret.connect(newStartState, oldStartState, '\0');
+            ret.addConnectionToEndStates(newStartState, '\0');
+            ret.endStates.Add(newStartState);
+            ret.startState = newStartState;
+            ret.hasEpsilons = true;
+            return ret;
+        }
+        public NFA conc(NFA second)
+        {
+            NFA ret = new NFA(this);
+            ret.incrementIndices(second.numStates);
+            List<int> oldEndStates = new List<int>(ret.endStates);
+            int secondStartState = second.startState;
+            foreach (Connection c in second.connections)
+            {
+                ret.connect(c.start, c.end, c.character);
+            }
+            ret.calculateConnectionPerState();
+            ret.addConnectionToEndStates(secondStartState, '\0');
+            foreach (int i in second.endStates)
+            {
+                ret.endStates.Add(i);
+            }
+            ret.hasEpsilons = true;
+            foreach (int i in oldEndStates)
+            {
+                ret.endStates.Remove(i);
+            }
+            return ret;
+        }
+        public void deEpsilonate()
+        {
+            List<Connection> epsilons = new List<Connection>();
+            foreach (Connection c in connections)
+            {
+                if (c.character == '\0')
+                {
+                    epsilons.Add(c);
+                }
+            }
+            hasEpsilons = epsilons.Count != 0;
+            while (hasEpsilons)
+            {
+                System.Console.WriteLine(epsilons.Count);
+                List<Connection> newEpsilons = new List<Connection>();
+                foreach (Connection c in epsilons)
+                {
+                    List<Connection> newConnections = new List<Connection>();
+                    foreach (Connection c2 in connectionPerState[c.end])
+                    {
+                        Connection newConnection = new Connection(c.start, c2.end, c2.character);
+                        if (c2.character == '\0')
+                        {
+                            newEpsilons.Add(newConnection);
+                        }
+                        if (endStates.Contains(c.end) && !endStates.Contains(c.start))
+                        {
+                            endStates.Add(c.start);
+                        }
+                        System.Console.WriteLine("Added {0} {1} {2}", c.start, c2.end, c2.character);
+                        newConnections.Add(newConnection);
+                    }
+                    foreach (Connection c2 in newConnections)
+                    {
+                        connections.Add(c2);
+                        connectionPerState[c2.start].Add(c2);
+                    }
+                    System.Console.WriteLine("Removed {0} {1} {2}", c.start, c.end, c.character);
+                    connections.Remove(c);
+                    connectionPerState[c.start].Remove(c);
+                }
+                epsilons = newEpsilons;
+                if (epsilons.Count == 0)
+                {
+                    hasEpsilons = false;
+                }
+            }
+        }
+        public bool recognizes(String str)
+        {
+            deEpsilonate();
+            List<int> currentStates = new List<int>();
+            currentStates.Add(startState);
+            for (int i = 0; i < str.Length; i++)
+            {
+                List<int> newStates = new List<int>();
+                foreach (int s in currentStates)
+                {
+                    foreach (Connection c in connectionPerState[s])
+                    {
+                        if (c.character == str[i])
+                        {
+                            newStates.Add(c.end);
+                        }
+                    }
+                }
+                currentStates = newStates;
+            }
+            foreach (int i in currentStates)
+            {
+                if (endStates.Contains(i))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ comp2014miniplTest/UnitTest1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using comp2014minipl;
+
+namespace comp2014miniplTest
+{
+    [TestClass]
+    public class NFATests
+    {
+        [TestMethod]
+        public void constructorWorks()
+        {
+            NFA test = new NFA("hello");
+            Assert.IsTrue(test.recognizes("hello"));
+            Assert.IsFalse(test.recognizes("hell"));
+            Assert.IsFalse(test.recognizes("helloo"));
+        }
+        [TestMethod]
+        public void orWorks()
+        {
+            NFA test = new NFA("hello");
+            NFA test2 = new NFA("lul");
+            NFA test3 = test.or(test2);
+            Assert.IsTrue(test3.recognizes("hello"), "Recognizes first alternative");
+            Assert.IsTrue(test3.recognizes("lul"), "Recognizes second alternative");
+            Assert.IsFalse(test3.recognizes("abc"), "Doesn't recognize random stuff");
+        }
+        [TestMethod]
+        public void closureWorks()
+        {
+            NFA test = new NFA("hello").closure();
+            Assert.IsTrue(test.recognizes(""), "recognizes 0 elements");
+            Assert.IsTrue(test.recognizes("hello"), "recognizes 1 element");
+            Assert.IsTrue(test.recognizes("hellohello"), "recognizes 2 elements");
+            Assert.IsTrue(test.recognizes("hellohellohellohellohello"), "recognizes 5 elements");
+            Assert.IsFalse(test.recognizes("abc"), "doesn't recognize random stuff");
+        }
+        [TestMethod]
+        public void closureWithOrWorks()
+        {
+            NFA a = new NFA("a");
+            NFA b = new NFA("b");
+            NFA test = a.or(b).closure();
+            Assert.IsTrue(test.recognizes("ababaabbbabaaabababba"));
+        }
+        [TestMethod]
+        public void concWorks()
+        {
+            NFA test = new NFA("hello");
+            NFA test2 = new NFA("lul");
+            NFA test3 = test.conc(test2);
+            Assert.IsTrue(test3.recognizes("hellolul"), "Recognizes correct concatenation");
+            Assert.IsFalse(test3.recognizes("lulhello"), "Doesn't recognize wrongly ordered concatenation");
+            Assert.IsFalse(test3.recognizes("lul"), "Doesn't recognize original second part");
+            Assert.IsFalse(test3.recognizes("hello"), "Doesn't recognize original first part");
+        }
+        [TestMethod]
+        public void concClosureWorks()
+        {
+            NFA hello = new NFA("hello");
+            NFA lul = new NFA("lul");
+            NFA test = lul.conc(hello.closure());
+            Assert.IsTrue(test.recognizes("lul"));
+            Assert.IsTrue(test.recognizes("lulhello"));
+            Assert.IsTrue(test.recognizes("lulhellohellohello"));
+            Assert.IsFalse(test.recognizes("abc"));
+        }
+        [TestMethod]
+        public void complexConcOrClosureWorks()
+        {
+            NFA hello = new NFA("hello");
+            NFA lul = new NFA("lul");
+            NFA test = hello.conc(lul.closure().or(hello)).conc(hello.closure());
+            Assert.IsTrue(test.recognizes("hellohellohello"));
+            Assert.IsTrue(test.recognizes("hellolullullul"));
+            Assert.IsTrue(test.recognizes("hellolullullulhellohello"));
+            Assert.IsTrue(test.recognizes("hellohello"));
+            Assert.IsTrue(test.recognizes("hello"));
+            Assert.IsFalse(test.recognizes("abc"));
+        }
+    }
+}
